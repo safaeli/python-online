4b3d2396b40c43e25f6248c34957bd0f525a1551
safaeli <safaeliash@gmail.com>
2025-10-16 11:22:32 +0800

    if else

---
+++ JAVASCRIPT/Oct15/script.js
@@ -0,0 +1,40 @@
+// let a = 5
+// console.log(a)
+
+// Q1 (odd/even number)
+// let num = prompt("Enter a number")
+// debugger;
+// if(num / 2 && num == 0){
+//     document.writeln("<h1>Its an even number</h1")
+// }else{
+//     document.writeln("<h1>Its an odd number</h1")
+// }
+// console.log(num)
+
+// Q2 (negative/positive number)
+// if(num >= 0){
+//     document.writeln("<h1>Its a positive number</h1")
+// }else{
+//     document.writeln("<h1>Its a negative number</h1")
+// }
+// console.log(num)
+
+// Q3 (eligibility check for driver's license)
+// let age = prompt("Enter your age")
+// if(age >= 17){
+//     document.writeln("<h1>You are eligible to get the license")
+// }else{
+//     document.writeln("<h1>You are not eligible to get the license")
+// }
+// console.log(age)
+
+// checking
+let b = 5;
+let c = 4;
+c++
+let f = c+b;
+f *= 3
+f++
+console.log(f>10)
+console.log(c<4)
+
