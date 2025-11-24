0e8f1e33d5b2862711a0fcc9bcbdb7109062bb75
safaeli <safaeliash@gmail.com>
2025-11-24 20:59:38 +0800

    intro, arithmetic practice

---
+++ PYTHON/Nov24(intro, arithmetic).py
@@ -0,0 +1,56 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a = b
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    a = b
+NameError: name 'b' is not defined
+a = 5
+b= 6
+print(a+b)
+11
+print(a-b)
+-1
+print(a/b)
+0.8333333333333334
+print(a%b)
+5
+>>> print(a*b)
+30
+>>> a = input("Enter a number")
+Enter a number 3
+>>> 
+>>> print(a)
+ 3
+>>> b = input("Enter a decimal number")
+Enter a decimal number3.4
+>>> print(a+b)
+ 33.4
+>>> a = int(input("Enter a num")
+...         4
+...         
+SyntaxError: '(' was never closed
+>>> a = int(input("Enter a num"))
+...         
+Enter a num2.3
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    a = int(input("Enter a num"))
+ValueError: invalid literal for int() with base 10: '2.3'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> a = int(input("Enter a num"))
+...         
+Enter a num2
+>>> b = float(input("Enter a dec"))
+...         
+Enter a dec4.5
+>>> print(a-b)
+...         
+-2.5
+>>> print(a+b)
+...         
+6.5
