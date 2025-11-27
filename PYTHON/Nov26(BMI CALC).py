73eee6a879666ce4a2d2594477da861571bbb628
safaeli <safaeliash@gmail.com>
2025-11-27 19:33:51 +0800

    bmi calc and input system

---
+++ PYTHON/Nov26(BMI CALC).py
@@ -0,0 +1,17 @@
+"""weight = int(input("Enter your weight:"))
+height = int(input("Enter your height:"))
+calc_BMI = weight/(height*height)
+print(calc_BMI)"""
+
+Name = input("Enter your name:")
+Age = int(input("Enter your age:"))
+Institution = input("Enter your school/college name:")
+Mark1 = float(input("Enter your 1st mark:"))
+Mark2 = float(input("Enter your 2nd mark:"))
+Mark3 = float(input("Enter your 3rd mark:"))
+Mark4 = float(input("Enter your 4th mark:"))
+Mark5 = float(input("Enter your 5th mark:"))
+total_mark = Mark1+Mark2+Mark3+Mark4+Mark5
+avg_mark = total_mark/5
+print("Total mark is :", total_mark)
+print("Average mark is :", avg_mark)
